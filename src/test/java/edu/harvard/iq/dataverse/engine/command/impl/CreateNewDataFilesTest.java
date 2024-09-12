1fae6d66055f476aa14dcc52d1d613a15de2e0a4
jo-pol <jo-pol@users.noreply.github.com>
2024-09-12 11:07:29 +0200

    cleanup

--- src/test/java/edu/harvard/iq/dataverse/engine/command/impl/CreateNewDataFilesTest.java
+++ src/test/java/edu/harvard/iq/dataverse/engine/command/impl/CreateNewDataFilesTest.java
@@ -14,23 +14,17 @@ import edu.harvard.iq.dataverse.util.testing.LocalJvmSettings;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -39,21 +33,22 @@ import static org.apache.commons.io.file.FilesUncheck.createDirectories;
 import static org.apache.commons.io.file.PathUtils.deleteDirectory;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 
 @LocalJvmSettings
 public class CreateNewDataFilesTest {
+    // TODO keep constants for annotations in sync with class name
+    Path testDir = Path.of("target/test/").resolve(getClass().getSimpleName());
+
     @BeforeEach
     public void cleanTmpDir() throws IOException {
-        var tmpDir = Paths.get("target/test/tmp");
-        if(tmpDir.toFile().exists())
-            deleteDirectory(tmpDir);
+        if(testDir.toFile().exists())
+            deleteDirectory(testDir);
     }
 
     @Test
-    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/tmp")
+    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/CreateNewDataFilesTest/tmp")
     public void execute_fails_to_upload_when_tmp_does_not_exist() throws FileNotFoundException {
 
         mockTmpLookup();
@@ -65,13 +60,13 @@ public class CreateNewDataFilesTest {
             .hasMessageContaining("Failed to save the upload as a temp file (temp disk space?)")
             .hasRootCauseInstanceOf(NoSuchFileException.class)
             .getRootCause()
-            .hasMessageStartingWith("target/test/tmp/temp/tmp");
+            .hasMessageStartingWith("target/test/CreateNewDataFilesTest/tmp/temp/tmp");
     }
 
     @Test
-    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/tmp")
+    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/CreateNewDataFilesTest/tmp")
     public void execute_fails_to_upload_too_big_files() throws FileNotFoundException {
-        createDirectories(Path.of("target/test/tmp/temp"));
+        createDirectories(Path.of("target/test/CreateNewDataFilesTest/tmp/temp"));
 
         mockTmpLookup();
         var cmd = createCmd("scripts/search/data/shape/shapefile.zip", mockDatasetVersion());
@@ -83,9 +78,9 @@ public class CreateNewDataFilesTest {
     }
 
     @Test
-    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/tmp")
+    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/CreateNewDataFilesTest/tmp")
     public void execute_fails_on_too_little_remaining_storage() throws FileNotFoundException {
-        createDirectories(Path.of("target/test/tmp/temp"));
+        createDirectories(Path.of("target/test/CreateNewDataFilesTest/tmp/temp"));
 
         mockTmpLookup();
         var cmd = createCmd("scripts/search/data/shape/shapefile.zip", mockDatasetVersion());
@@ -100,9 +95,9 @@ public class CreateNewDataFilesTest {
     }
 
     @Test
-    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/tmp")
+    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/CreateNewDataFilesTest/tmp")
     public void execute_succeeds() throws FileNotFoundException, CommandException {
-        var tempDir = Path.of("target/test/tmp/temp");
+        var tempDir = testDir.resolve("tmp/temp");
         var testFile = "scripts/search/data/binary/trees.zip";
         createDirectories(tempDir);
 
@@ -128,17 +123,19 @@ public class CreateNewDataFilesTest {
     }
 
     @Test
-    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/tmp")
+    @JvmSetting(key = JvmSettings.FILES_DIRECTORY, value = "target/test/CreateNewDataFilesTest/tmp")
     public void execute_with_2_shapes() throws Exception {
-        var tempDir = Path.of("target/test/tmp/temp");
-        List<String> file_names = Arrays.asList("shape1.shp", "shape1.shx", "shape1.dbf", "shape1.prj", "shape1.fbn", "shape1.fbx", // 1st shapefile
-            "shape2.shp", "shape2.shx", "shape2.dbf", "shape2.prj",     // 2nd shapefile
-            "shape2.txt", "shape2.pdf", "shape2",                  // single files, same basename as 2nd shapefile
-            "README.MD", "shp_dictionary.xls", "notes"); //, "prj");                  // single files
-        File testFile = createAndZipFiles(file_names, "two-shapes.zip");
+        var tempDir = testDir.resolve("tmp/temp");
+        createDirectories(tempDir);
+
+        File testFile = createAndZipFiles(Arrays.asList(
+            "shape1.shp", "shape1.shx", "shape1.dbf", "shape1.prj", "shape1.fbn", "shape1.fbx", // 1st shapefile
+            "shape2.shp", "shape2.shx", "shape2.dbf", "shape2.prj", // 2nd shapefile
+            "shape2.txt", "shape2.pdf", "shape2",                   // single files, same basename as 2nd shapefile
+            "README.MD", "shp_dictionary.xls", "notes"              // single files
+        ), testDir.resolve("shapes.zip"));
 
         // TODO mock CDI provider to allow mime type check
-        createDirectories(tempDir);
 
         mockTmpLookup();
         var cmd = createCmd(testFile.toString(), mockDatasetVersion());
@@ -163,20 +160,16 @@ public class CreateNewDataFilesTest {
         }
     }
 
-    @TempDir
-    Path tempFolder;
-
     // simplified version from ShapefileHandlerTest
-    private File createAndZipFiles(List<String> file_names, String zipfile_name) throws IOException {
-        var zip_file_obj = tempFolder.resolve(zipfile_name).toFile();
-        try (ZipOutputStream zip_stream = new ZipOutputStream(new FileOutputStream(zip_file_obj))) {
+    private File createAndZipFiles(List<String> file_names, Path zipfile) throws IOException {
+        try (ZipOutputStream zip_stream = new ZipOutputStream(new FileOutputStream(zipfile.toFile()))) {
             for (var file_name : file_names) {
                 zip_stream.putNextEntry(new ZipEntry(file_name));
                 zip_stream.write("content".getBytes(), 0, 7);
                 zip_stream.closeEntry();
             }
         }
-        return zip_file_obj;
+        return zipfile.toFile();
     }
 
     private static @NotNull CreateNewDataFilesCommand createCmd(String name, DatasetVersion dsVersion) throws FileNotFoundException {
