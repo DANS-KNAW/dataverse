8058e5bca62b981e62ec93b4e2b2302c6b08fbe4
jo-pol <jo-pol@users.noreply.github.com>
2025-08-14 09:18:11 +0200

    reverted not intended change

--- src/main/java/edu/harvard/iq/dataverse/Dataset.java
+++ src/main/java/edu/harvard/iq/dataverse/Dataset.java
@@ -700,6 +700,7 @@ public class Dataset extends DvObjectContainer {
         }
         return 0;
     }
+
     public String getCitation() {
         return getCitation(false, getLatestVersion());
     }
