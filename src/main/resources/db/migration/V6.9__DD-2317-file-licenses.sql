c77d556468197bdde3b7ac959880084d031a43e0
jo-pol <jo-pol@users.noreply.github.com>
2026-06-09 14:01:50 +0200

    more foreign keys

--- src/main/resources/db/migration/V6.9__DD-2317-file-licenses.sql
+++ src/main/resources/db/migration/V6.9__DD-2317-file-licenses.sql
@@ -35,11 +35,11 @@ INSERT INTO termsofaccess (id, availabilitystatus, contactforaccess, confidentia
 SELECT id, availabilitystatus, contactforaccess, confidentialitydeclaration, dataaccessplace, originalarchive, sizeofcollection, studycompletion, termsofaccess, fileaccessrequest
 FROM termsofuseandaccess WHERE id NOT IN (SELECT id FROM termsofaccess);
 
-ALTER TABLE datasetversion ADD COLUMN IF NOT EXISTS termsofaccess_id BIGINT;
-ALTER TABLE datasetversion ADD COLUMN IF NOT EXISTS  default_termsofuseorlicense_id BIGINT;
+ALTER TABLE datasetversion ADD COLUMN IF NOT EXISTS termsofaccess_id BIGINT REFERENCES termsofaccess(id);
+ALTER TABLE datasetversion ADD COLUMN IF NOT EXISTS default_termsofuseorlicense_id BIGINT REFERENCES termsofuseorlicense(id);
 
-ALTER TABLE template ADD COLUMN IF NOT EXISTS termsofaccess_id BIGINT;
-ALTER TABLE template ADD COLUMN IF NOT EXISTS  default_termsofuseorlicense_id BIGINT;
+ALTER TABLE template ADD COLUMN IF NOT EXISTS termsofaccess_id BIGINT REFERENCES termsofaccess(id);
+ALTER TABLE template ADD COLUMN IF NOT EXISTS  default_termsofuseorlicense_id BIGINT REFERENCES termsofuseorlicense(id);
 
 ALTER TABLE filemetadata ADD COLUMN IF NOT EXISTS termsofuseorlicense_id BIGINT;
 
@@ -49,18 +49,20 @@ DO $$
             UPDATE datasetversion SET termsofaccess_id = termsofuseandaccess_id, default_termsofuseorlicense_id = termsofuseandaccess_id;
             ALTER TABLE datasetversion DROP COLUMN termsofuseandaccess_id;
             ALTER TABLE datasetversion ALTER COLUMN termsofaccess_id SET NOT NULL;
+            ALTER TABLE datasetversion ALTER COLUMN default_termsofuseorlicense_id SET NOT NULL;
         END IF;
 
         IF EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name='template' and column_name='termsofuseandaccess_id') THEN
             UPDATE template SET termsofaccess_id = termsofuseandaccess_id, default_termsofuseorlicense_id = termsofuseandaccess_id;
             ALTER TABLE template DROP COLUMN termsofuseandaccess_id;
             ALTER TABLE template ALTER COLUMN termsofaccess_id SET NOT NULL;
+            ALTER TABLE template ALTER COLUMN default_termsofuseorlicense_id SET NOT NULL;
         END IF;
     END
 $$;
---------------------------------------------------------------------------
--- Perhaps less code breaks with the view, no updates are possible though.
---------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+-- Perhaps less code breaks with the view during development, no updates are possible though.
+---------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS termsofuseandaccess;
 CREATE VIEW termsofuseandaccess AS
 SELECT
