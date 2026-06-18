d6aa885d714affafd7248d4793351088e3ecf03d
jo-pol <jo-pol@users.noreply.github.com>
2026-06-18 10:30:36 +0200

    revisited DatasetVersionDifference

--- src/main/java/edu/harvard/iq/dataverse/DatasetVersionDifference.java
+++ src/main/java/edu/harvard/iq/dataverse/DatasetVersionDifference.java
@@ -207,40 +207,25 @@ public final class DatasetVersionDifference {
                 int b = dsfb[0].getDatasetFieldType().getMetadataBlock().getId().intValue();
             return Integer.valueOf(a).compareTo(b);
         });
-        getTermsDifferences();
+        getTermsOfAccessDifferences();
+        getTermsOfUseAndLicenseDifferences();
     }
     
 
        
-    private void getTermsDifferences() {
+    private void getTermsOfAccessDifferences() {
 
-        TermsOfUseAndAccess originalTerms = originalVersion.getTermsOfUseAndAccess();
+        TermsOfAccess originalTerms = originalVersion.getTermsOfAccess();
         if(originalTerms == null) {
-            originalTerms = new TermsOfUseAndAccess();
+            originalTerms = new TermsOfAccess();
         }
         // newTerms should never be null
-        TermsOfUseAndAccess newTerms = newVersion.getTermsOfUseAndAccess();
+        TermsOfAccess newTerms = newVersion.getTermsOfAccess();
         if(newTerms == null) {
             logger.warning("New version does not have TermsOfUseAndAccess");
-            newTerms = new TermsOfUseAndAccess();
+            newTerms = new TermsOfAccess();
         }
         
-        checkAndAddToChangeList(originalTerms.getTermsOfUse(), newTerms.getTermsOfUse(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.header"));
-        checkAndAddToChangeList(originalTerms.getConfidentialityDeclaration(), newTerms.getConfidentialityDeclaration(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.declaration"));
-        checkAndAddToChangeList(originalTerms.getSpecialPermissions(), newTerms.getSpecialPermissions(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.permissions"));
-        checkAndAddToChangeList(originalTerms.getRestrictions(), newTerms.getRestrictions(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.restrictions"));
-        checkAndAddToChangeList(originalTerms.getCitationRequirements(), newTerms.getCitationRequirements(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.citationRequirements"));
-        checkAndAddToChangeList(originalTerms.getDepositorRequirements(), newTerms.getDepositorRequirements(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.depositorRequirements"));
-        checkAndAddToChangeList(originalTerms.getConditions(), newTerms.getConditions(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.conditions"));
-        checkAndAddToChangeList(originalTerms.getDisclaimer(), newTerms.getDisclaimer(),
-                BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.disclaimer"));
         checkAndAddToChangeList(originalTerms.getTermsOfAccess(), newTerms.getTermsOfAccess(),
                 BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfAccess.termsOfsAccess"));
         checkAndAddToChangeList(originalTerms.getDataAccessPlace(), newTerms.getDataAccessPlace(),
@@ -256,7 +241,38 @@ public final class DatasetVersionDifference {
         checkAndAddToChangeList(originalTerms.getStudyCompletion(), newTerms.getStudyCompletion(),
                 BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfAccess.addInfo.studyCompletion"));
     }
-    
+
+    private void getTermsOfUseAndLicenseDifferences() {
+
+        TermsOfUseAndLicense originalTerms = originalVersion.getTermsOfUseAndLicense();
+        if(originalTerms == null) {
+            originalTerms = new TermsOfUseAndLicense();
+        }
+        // newTerms should never be null
+        TermsOfUseAndLicense newTerms = newVersion.getTermsOfUseAndLicense();
+        if(newTerms == null) {
+            logger.warning("New version does not have TermsOfUseAndAccess");
+            newTerms = new TermsOfUseAndLicense();
+        }
+
+        checkAndAddToChangeList(originalTerms.getTermsOfUse(), newTerms.getTermsOfUse(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.header"));
+        checkAndAddToChangeList(originalTerms.getConfidentialityDeclaration(), newTerms.getConfidentialityDeclaration(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.declaration"));
+        checkAndAddToChangeList(originalTerms.getSpecialPermissions(), newTerms.getSpecialPermissions(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.permissions"));
+        checkAndAddToChangeList(originalTerms.getRestrictions(), newTerms.getRestrictions(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.restrictions"));
+        checkAndAddToChangeList(originalTerms.getCitationRequirements(), newTerms.getCitationRequirements(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.citationRequirements"));
+        checkAndAddToChangeList(originalTerms.getDepositorRequirements(), newTerms.getDepositorRequirements(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.depositorRequirements"));
+        checkAndAddToChangeList(originalTerms.getConditions(), newTerms.getConditions(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.conditions"));
+        checkAndAddToChangeList(originalTerms.getDisclaimer(), newTerms.getDisclaimer(),
+            BundleUtil.getStringFromBundle("file.dataFilesTab.terms.list.termsOfUse.addInfo.disclaimer"));
+    }
+
     private void checkAndAddToChangeList(String originalTerm, String newTerm,
             String termLabel) {
         originalTerm = StringUtil.nullToEmpty(originalTerm);
